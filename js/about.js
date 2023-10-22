ac5ec0955feac9aa9a8dbd357f101aa19666d1fa
Francesco Montanaro <francesco.montanaro998@gmail.com>
2023-10-22 15:22:04 +0200

    website updated

--- js/about.js
+++ js/about.js
@@ -1,39 +1,21 @@
 let string = '"Computers are incredibly fast, accurate and stupid; humans are incredibly slow, inaccurate and brilliant; together they are powerful beyond imagination."';
-let string_chars = string.split("");
-let shell = document.getElementById("shell");
 
-
-function write_to_shell(shell, string_chars){
-    let iterator = 0;
-    shell.innerHTML = "|";
-
-    const interval = setInterval(function(){
-    if(iterator >= string_chars.length){
-        clearInterval(interval);
-
-        let dash_iterations = 0;
-        const dash_interval = setInterval(function(){
-            if(dash_iterations % 2 == 0)
-                shell.innerHTML = string.substring(0, string.length);
-            else
-                shell.innerHTML = shell.innerHTML + "|";
-            if(dash_iterations >= 2){
-                clearInterval(dash_interval);
-                setTimeout(function(){
-                    shell.innerHTML += "<br>- Albert Einstein";
-                }, 500);
-            }
-            dash_iterations ++;
-        },500);
+document.addEventListener('DOMContentLoaded', () => {
+    const shell = document.getElementById('shell');
+    let index = 0;
+  
+    function type() {
+      if (index < string.length) {
+        shell.textContent += string[index];
+        index++;
+        setTimeout(type, 100);
+      }
+      else{
+        setTimeout(function(){
+            shell.innerHTML += "<br>- Albert Einstein";
+        }, 500);
+      }
     }
-    else{
-        shell.innerHTML = string.substring(0, iterator + 1) + "|";
-        iterator ++;
-    }
-    },100);
-}
-
-write_to_shell(shell, string_chars);
-setInterval(function(){
-    write_to_shell(shell, string_chars);
-}, 25000);
+  
+    type();
+});
